08308523663fa9de576ef1c954c4726c0db7cac8
tastytim <tastytimgm@gmail.com>
2021-11-26 16:51:26 +0100

    commit 1

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 // Creare una variabile con un paragrafo di testo a vostra scelta.
 $xParagrafo="Una variabile con un paragrafo.";
 // Una parola da censurare viene passata dall’utente tramite parametro GET.
-$NAME = $_GET['NAME'];
+$name = $_GET["firstName"];
+echo $name;
+var_dump($name);
 
 
 ?>
 
 
 <!DOCTYPE html>
-<html>
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <head>
-
+<title>PHP</title>
     </head>
     <body>
         <header></header>
@@ -19,7 +23,7 @@ $NAME = $_GET['NAME'];
             <!-- Stampare a schermo il paragrafo e la sua lunghezza. -->
             <p><?php echo $xParagrafo . " Length = " .strlen($xParagrafo)?></p>
             <!-- Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
-           
+            <p><?php echo $name ?></p>
         </main>
         <footer></footer>
     </body>
