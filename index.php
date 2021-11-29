e62cfd6125ca08113a029b4b7362910348a792d2
tastytim <tastytimgm@gmail.com>
2021-11-29 20:17:38 +0100

    fixed str_replace

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 // Creare una variabile con un paragrafo di testo a vostra scelta.
-$xParagrafo="Una variabile con un paragrafo.";
+$xParagrafo="Una variabile con un paragrafo.Una variabile con un paragrafo.";
 // Una parola da censurare viene passata dall’utente tramite parametro GET.
+$wordToDelete = $_GET["word"];
+// dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare
+$xNewParagrafo = str_replace($wordToDelete , "***" , $xParagrafo);
+$lengthNewParagraph = strlen($xNewParagrafo);
 ?>
 
 
@@ -17,8 +21,9 @@ $xParagrafo="Una variabile con un paragrafo.";
         <main>
             <!-- Stampare a schermo il paragrafo e la sua lunghezza. -->
             <p><?php echo $xParagrafo . " Length = " .strlen($xParagrafo)?></p>
-            <!-- Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
-            <p><?php echo substr_replace($_GET["variabile"] , "***", 0) . " Length = " . strlen(substr_replace($_GET["variabile"] , "***", 0)) ?></p>
+            <!-- Stampare di nuovo il paragrafo e la sua lunghezza-->
+            <p><?php 
+            echo $xNewParagrafo . " Lenght = " . $lengthNewParagraph ?></p>
         </main>
         <footer></footer>
     </body>
